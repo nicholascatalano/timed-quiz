c77f2efcee0d41cf728167ecea3dcf52e40b9d72
nickcatalano5 <nickcatalano24@gmail.com>
2023-11-28 15:32:50 -0500

    added timer to javascript and displayed it on page

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,12 @@ var questionsCard = document.querySelector("#questionsCard");
 var wrapper = document.querySelector(".wrapper");
 
 // DATA
+// starting score variable
 var score = 0;
+// variable to determine what question to start on, 0 in this case
 var questionIndex = 0;
 
+// questions array
 var questions = [
   {
     question: "What color is a banana?",
@@ -31,5 +34,44 @@ var questions = [
   },
 ];
 
-var secondsLeft = 75
-var subtractSeconds = 10
+// variable for starting timer count
+var secondsLeft = 5;
+// variable for how much time to subtract from timer count
+var subtractSeconds = 10;
+// variable to add list element
+var ulAdd = document.createElement("ul");
+// variable to check time is at 0
+var zeroTime = 0;
+
+//FUNCTIONS
+// renders questions and answers to page depending on index
+function render(questionIndex) {
+  //clear page of current card and list
+  questionsCard.innerHTML = "";
+  ulAdd.innerHTML = "";
+}
+
+//USER INPUT
+// clicks start button, timer begins, and first question renders
+startTimer.addEventListener("click", function () {
+  if (zeroTime === 0) {
+    zeroTime = setInterval(function () {
+      secondsLeft--;
+      timeLeft.textContent = "Time remaining: " + secondsLeft;
+
+      if (secondsLeft <= 0) {
+        clearInterval(zeroTime);
+        quizOver();
+        timeLeft.textContent = "Game Over!";
+      }
+    }, 1000);
+  }
+  render(questionIndex);
+});
+// click one of the choices
+// enter innitials on final score screen
+// click submit on final score screen
+// click clear highscore on highscores screen
+// click go back on highscore screen
+
+//INITIALIZATION
