fe8240d7966758157297b4f0431a1be6ee5d99f5
nickcatalano5 <nickcatalano24@gmail.com>
2023-11-28 13:07:33 -0500

    added dependencies

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// DEPENDENCIES
+var timeLeft = document.querySelector("#timeLeft");
+var startTimer = document.querySelector("#startQuiz");
+var questionsCard = document.querySelector("#questionsCard");
+var wrapper = document.querySelector(".wrapper");
+
+// DATA
+var score = 0;
+var question
